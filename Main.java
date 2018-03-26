8f9babfc39a6a089a6aed6be739ece141df6b802
john warui <kihatowarui@outlook.com>
2018-03-26 09:23:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package learn;
+import javax.swing.JFrame;
+
+
+public class Main extends  JFrame{
+
+    public  Main(){
+
+        setSize(300,200);
+        setTitle("Simple");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        Main simple = new Main();
+
+
+        simple.setVisible(true);
+
+
+	// write your code here
+    }
+}
